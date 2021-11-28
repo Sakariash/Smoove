44412d21c39fe3fc5a8fe4ada9b74f67f2e02627
amandahulten <amahul1029@skola.goteborg.se>
2021-11-28 14:03:09 +0100

    added responsive navbar

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
+// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
 
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
+//     document.querySelector(this.getAttribute("href")).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav_menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
